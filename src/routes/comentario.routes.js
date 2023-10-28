e9f22b13e231765fdcf4f6067d3c859e7f73af6b
evilBetooxx <albertof4in@gmail.com>
2023-10-28 16:34:28 -0600

    crud - comentario

---
+++ src/routes/comentario.routes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+    getComentarios,
+    getComentario,
+    getComentariosByPublicacion,
+    createComentario,
+    updateComentario,
+    deleteComentario,
+    deleteComentarioByFechaAndPublicacion
+} from "../controllers/comentario.controller.js"; 
+
+const router = express.Router();
+
+router.get("/comentarios", getComentarios); 
+router.get("/comentarios/:id", getComentario); 
+router.post("/comentarios-publicacion", getComentariosByPublicacion);
+router.post("/comentarios", createComentario); 
+router.put("/comentarios/:id", updateComentario); 
+router.delete("/comentarios/:id", deleteComentario); 
+router.delete("/comentarios-fecha-publicacion", deleteComentarioByFechaAndPublicacion)
+
+export default router;
